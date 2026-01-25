780925858b29fc2f423cccc010c6e13a56d16898
ben.read <endymion1818@gmail.com>
2026-01-25 15:49:51 +0000

    feat: [WIP] fetch homepage content

--- src/components/datocms/connector.js
+++ src/components/datocms/connector.js
@@ -1,19 +1,69 @@
 import { executeQuery } from "@datocms/cda-client";
 
-const query = `
-  query {
-    allArticles {
+const queries = {
+  fetchAllPages: `
+  {
+    allPages {
       id
       title
+      _status
+      _firstPublishedAt
+      slug
+    }
+    _allPagesMeta {
+      count
     }
   }
-`;
-
-const result = await executeQuery(query, {
-  token: import.meta.env.DATOCMS_CDA_TOKEN,
-  options: {
-    fetchFn: globalThis.fetch
+`,
+  fetchHomePage: `
+  {
+    homepage {
+      introTitle
+      introFeaturedImage {
+        url
+        alt
+      }
+      introStructuredText {
+        value
+        links {
+          slug
+        }
+      }
+      aboutTitle
+      aboutFeaturedImage {
+        url
+        alt
+      }
+      aboutStructuredText {
+        value
+        links {
+          slug
+        }
+      }
+    }
   }
-});
+`,
+};
 
-console.log(result);
+/**
+ *
+ * @param {string} queryType
+ * @returns {Promise<string>}
+ */
+async function retrieveData(queryType) {
+  console.log({ queryType });
+
+  const query = queries[queryType];
+
+  if (!query) return;
+
+  const result = await executeQuery(query, {
+    token: import.meta.env.DATOCMS_CDA_TOKEN,
+  });
+
+  console.log({ result });
+
+  return result;
+}
+
+export default retrieveData;
